ed8bf81e50370170cd726d69c25fdd8c026e146a
Rahul <balanrahul59@gmail.com>
2023-03-04 14:57:24 +0530

    initializing student load Back end

---
+++ studentloan/src/main/java/com/studentloan/studentloan/StudentloanApplication.java
@@ -0,0 +1,13 @@
+package com.studentloan.studentloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentloanApplication.class, args);
+	}
+
+}
---
+++ studentloan/src/test/java/com/studentloan/studentloan/StudentloanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentloan.studentloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
