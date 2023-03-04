7c04de3078dddddf98e2170c0348ee848f2dc049
Rahul <balanrahul59@gmail.com>
2023-03-04 15:06:13 +0530

    testing project

--- studentloan/src/main/java/com/studentloan/studentloan/StudentloanApplication.java
+++ studentloan/src/main/java/com/studentloan/studentloan/StudentloanApplication.java
@@ -8,6 +8,7 @@ public class StudentloanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentloanApplication.class, args);
+		System.out.println("Hello Word");
 	}
 
 }
